9445307583ee5e0c402d88ccb9975f43824b0e50
Filip Dimitrov <github@filip-dimitrov.eu>
2020-09-18 16:31:38 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider freeipa {
+  #  host = "demo1.freeipa.org"
+  host     = "ipa.demo1.freeipa.org"
+  username = "admin" # or set $FREEIPA_USERNAME
+  password = "Secret123"
+  insecure = true
+}
+
+resource freeipa_host "foo" {
+  fqdn        = "foo.example.test"
+  description = "This is my foo host"
+  force       = true
+  random      = true
+}
+
+resource freeipa_host "bar" {
+  fqdn         = "bar.example.test"
+  userpassword = "abcde"
+  force        = true
+}
