756caff8547d27795adbced94120d0efa52d6015
Filip Dimitrov <github@filip-dimitrov.eu>
2020-09-18 16:47:47 +0300

    yolo

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     freeipa = {
-      source = "freeipa"
-      #      source  = "-/freeipa"
+      source = "-/freeipa"
       #      source  = "camptocamp/freeipa"
       version = "0.6.0"
     }
